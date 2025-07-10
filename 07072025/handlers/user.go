f6a71b394f2bdd6b7a97500e93e79524187978f4
Asif K A <asifpbvr@gmail.com>
2025-07-10 23:29:39 +1100

    bug fixing in handler

--- 07072025/handlers/user.go
+++ 07072025/handlers/user.go
@@ -2,24 +2,28 @@ package handlers
 
 import (
 	"07072025/models"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
 
-var va *validator.Validate
+var va = validator.New()
 
 func RegisterUser(c *gin.Context) {
 	var input models.RegisterInput
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"errot": "invalid input"})
-
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
 	}
+
+	fmt.Println(input)
 
 	if err := va.Struct(input); err != nil {
 		ve := err.(validator.ValidationErrors)
 		c.JSON(http.StatusBadRequest, gin.H{"validation_error": ve.Error()})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "user registered successfully"})
